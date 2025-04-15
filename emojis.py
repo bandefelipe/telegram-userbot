dada94794612b72ef724edc35ec0a6755d5d3e21
bandefelipe <bandefelipe@gmail.com>
2025-04-15 12:27:53 -0300

    Create emojis.py
    emojis

---
+++ emojis.py
@@ -0,0 +1,13 @@
+EMOJIS = {
+    "dinheiro": "<emoji id=5409048419211682843>",
+    "sirene_verde": "<emoji id=5213446022184840227>",
+    "sinal_verde": "<emoji id=5215522595922779944>",
+    "seta_verde_direita": "<emoji id=5215174853895660531>",
+    "presente": "<emoji id=5203996991054432397>",
+    "carregando": "<emoji id=5386367538735104399>",
+    "bolsa_dinheiro": "<emoji id=5224257782013769471>",
+    "fogo": "<emoji id=5424972470023104089>",  # ID correto do 🔥
+    "maior_18": "<emoji id=5348485489097717994>",  # ID correto do 🔞
+    "dedo_direita": "👉",  # padrão
+    "dedo_esquerda": "👈"   # padrão
+}
