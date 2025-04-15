2cd53f907142716a3a337ae7731a0e16689c62db
bandefelipe <bandefelipe@gmail.com>
2025-04-15 15:16:32 -0300

    Update emojis.py
    att emojis

--- emojis.py
+++ emojis.py
@@ -1,12 +1,12 @@
 EMOJIS = {
-    "dinheiro": "<emoji id=5409048419211682843>",
-    "sirene_verde": "<emoji id=5213446022184840227>",
-    "sinal_verde": "<emoji id=5215522595922779944>",
-    "seta_verde_direita": "<emoji id=5215174853895660531>",
-    "presente": "<emoji id=5203996991054432397>",
-    "carregando": "<emoji id=5386367538735104399>",
-    "bolsa_dinheiro": "<emoji id=5224257782013769471>",
-    "fogo": "<emoji id=5424972470023104089>",  # ID correto do 🔥
-    "maior_18": "<emoji id=5348485489097717994>",  # ID correto do 🔞
-    "bola_pulando": "<emoji id=5416081784641168838>",  # ID correto do 🔞
+    "dinheiro": "<emoji id=5409048419211682843>💰</emoji>",
+    "sirene_verde": "<emoji id=5213446022184840227>🚨</emoji>",
+    "sinal_verde": "<emoji id=5215522595922779944>✅</emoji>",
+    "seta_verde_direita": "<emoji id=5215174853895660531>➡️</emoji>",
+    "presente": "<emoji id=5203996991054432397>🎁</emoji>",
+    "carregando": "<emoji id=5386367538735104399>⏳</emoji>",
+    "bolsa_dinheiro": "<emoji id=5224257782013769471>💼</emoji>",
+    "fogo": "<emoji id=5424972470023104089>🔥</emoji>",
+    "maior_18": "<emoji id=5348485489097717994>🔞</emoji>",
+    "bola_pulando": "<emoji id=5416081784641168838>🏀</emoji>",
 }
